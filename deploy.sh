fea7dfb5fbeb5536215a6dc5ac7faf1caeeaad7d
Jürgen Ryannel <juergen@ryannel.org>
2022-03-05 20:12:02 +0100

    add cname and nojekyll

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ yarn run docs:build
 cd docs/.vuepress/dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'objectlinkprotocol.net' > CNAME
 
 git init
 git add -A
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:apigear-io/objectlink-site.git master:gh-pages
+git push -f git@github.com:apigear-io/objectlink-spec.git main:gh-pages
 
 cd -
